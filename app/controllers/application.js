7f7b3c752cc77f46de24b11cb83a0319bb2be8ce
Jordan Morano <jordan.morano@gmail.com>
2020-07-29 17:37:48 -0400

    Add reset button

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,13 @@
 import Controller from '@ember/controller';
+import { action, set } from '@ember/object';
 
 export default class ApplicationController extends Controller {
   queryParams = ['q'];
 
   q = '';
+
+  @action
+  reset() {
+    set(this, 'q', '');
+  }
 }
