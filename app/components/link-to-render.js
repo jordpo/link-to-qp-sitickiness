80cc7f20cdb71af6fa9c7b6f6f1639687087d195
Jordan Morano <jordan.morano@gmail.com>
2020-07-29 18:11:29 -0400

    Add fix for rendering href

--- app/components/link-to-render.js
+++ app/components/link-to-render.js
@@ -2,13 +2,29 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { getOwner } from '@ember/application';
+import { computed } from '@ember/object';
+import { next } from '@ember/runloop';
 
 export default class LinkToRender extends Component {
   @service router;
   @tracked href;
 
+  get postsController() {
+    return getOwner(this).lookup('controller:posts');
+  }
+
+
+  @computed('postsController.q')
+  get q() {
+    const postsController = getOwner(this).lookup('controller:posts');
+    return postsController.q;
+  }
+
   @action
   grabHref(el) {
-    this.href = el.getAttribute('href');
+    next(this, function() {
+      this.href = el.getAttribute('href');
+    });
   }
 }
